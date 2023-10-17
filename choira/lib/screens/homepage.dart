981718fa8faf02f5d3d16a7a0e811e8238b12305
prashantk96 <prashantkirpan96@gmail.com>
2023-10-17 16:36:58 +0530

    final commit

--- choira/lib/screens/homepage.dart
+++ choira/lib/screens/homepage.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
+  List Images = [
+    "assets/images/homepage/unsplash-b35lgevmtw.png",
+    "assets/images/homepage/bulb-1.png",
+    "assets/images/homepage/mask-group-Xep.png",
+    "assets/images/homepage/startup-1.png",
+    "assets/images/homepage/mask-group.png"
+  ];
+
+  List Images2 = [
+    "assets/images/homepage/xfsbw8w400x400-1.png",
+    "assets/images/homepage/rectangle-15.png",
+    "assets/images/homepage/rectangle-15-Q5v.png"
+  ];
+
+  List Text1 = [
+    "Learn about the Beats",
+    "Welcome to Music",
+    "Learn basic song writing"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff0a1733),
       appBar: AppBar(
         backgroundColor:
             const Color(0xff0a1733), // Set the AppBar background color
@@ -20,20 +41,258 @@ class Homepage extends StatelessWidget {
               // Handle notification button press
             },
           ),
-          CustomAvatar(),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: const CircleAvatar(
+              backgroundImage:
+                  AssetImage('assets/images/homepage/ellipse-1-bg.png'),
+              radius: 20,
+            ),
+          )
         ],
       ),
-      body: HomePageBody(),
+      body: Column(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'What are you going to learn today?',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                hintStyle: const TextStyle(color: Colors.white),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 25,
+            child: const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8, left: 8),
+                  child: Text(
+                    'Courses',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8, right: 8),
+                  child: Text(
+                    'view all',
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 300,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: Images.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 68,
+                        height: 68,
+                        margin: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.yellow,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
+                            Images[index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  height: 25,
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 8, left: 8),
+                        child: Text(
+                          'Learn for free',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(
+                            Images2.length,
+                            (index) => Container(
+                              width: 5,
+                              height: 5,
+                              // margin: EdgeInsets.all(5),
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color(0xffFFC701),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 190,
+                  child: Expanded(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: Images2.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                height: 95,
+                                width: 154,
+                                margin: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xff0a1733),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: Column(children: [
+                                    Image.asset(
+                                      Images2[index],
+                                      height: 95,
+                                      width: 154,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        Text1[index],
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.normal,
+                                          fontStyle: FontStyle.italic,
+                                        ),
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Center(
+                                      child: Container(
+                                          height: 18,
+                                          width: 90,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(15),
+                                            color:
+                                                Colors.yellow.withOpacity(0.5),
+                                          ),
+                                          child: Center(
+                                            child: Text(
+                                              'Introduction',
+                                              style: TextStyle(
+                                                  color: Colors.yellow),
+                                            ),
+                                          )),
+                                    )
+                                  ]),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.green.withOpacity(0.3),
+                    Colors.black.withOpacity(0.3),
+                    Colors.red.withOpacity(0.3),
+                  ],
+                )),
+            margin: const EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: Image.asset(
+                        'assets/images/homepage/lock-and-key-1.png'),
+                  ),
+                ),
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      'Access more than 700 courses by purchasing a plan',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        selectedItemColor: Color(0xFFFFC701),
-        backgroundColor: Color(0xff0a1733),
+        selectedItemColor: const Color(0xFFFFC701),
+        backgroundColor: const Color(0xff0a1733),
         useLegacyColorScheme: true,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(
               Icons.home,
-              color: Colors.white,
+              color: Color(0xFFFFC701),
             ),
             label: '',
           ),
@@ -48,213 +307,6 @@ class Homepage extends StatelessWidget {
           BottomNavigationBarItem(
             icon: Icon(Icons.school_outlined, color: Colors.white),
             label: '',
-          ),
-        ],
-      ),
-      backgroundColor: const Color(0xff0a1733),
-    );
-  }
-}
-
-class CustomAvatar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      child: const CircleAvatar(
-        backgroundImage: AssetImage('assets/images/homepage/ellipse-1-bg.png'),
-        radius: 20,
-      ),
-    );
-  }
-}
-
-class HomePageBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text(
-            'What are you going to learn today?',
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Search',
-              hintStyle: const TextStyle(color: Colors.white),
-              prefixIcon: const Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(40.0),
-              ),
-            ),
-          ),
-        ),
-        HorizontalScrollList(
-          isFirstList: true,
-          listText: 'List 1',
-        ),
-        HorizontalScrollList(
-          isSecondList: true,
-          listText: 'List 2',
-        ),
-        CardWidget(),
-      ],
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class HorizontalScrollList extends StatelessWidget {
-  final bool isFirstList;
-  final bool isSecondList;
-  final String listText;
-
-  List Images = [
-    "assets/images/homepage/unsplash-b35lgevmtw.png",
-    "assets/images/homepage/bulb-1.png",
-    "assets/images/homepage/mask-group-Xep.png",
-    "assets/images/homepage/startup-1.png",
-    "assets/images/homepage/mask-group.png"
-  ];
-
-  HorizontalScrollList(
-      {this.isFirstList = false,
-      this.isSecondList = false,
-      this.listText = '123'});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          if (isFirstList)
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Courses',
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ),
-          if (isSecondList)
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Learn for free',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: Images.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: 68,
-                  height: 68,
-                  margin: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.asset(
-                      Images[index],
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          if (isSecondList)
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  Images.length,
-                  (index) => Container(
-                    width: 10,
-                    height: 10,
-                    margin: const EdgeInsets.all(5),
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xff14203B),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          if (!isFirstList && !isSecondList)
-            Text(
-              listText,
-              style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-class CardWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Colors.green.withOpacity(0.3),
-              Colors.black.withOpacity(0.3),
-              Colors.red.withOpacity(0.3),
-            ],
-          )),
-      margin: const EdgeInsets.all(16.0),
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 100,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 40),
-              child: Image.asset('assets/images/homepage/lock-and-key-1.png'),
-            ),
-          ),
-          const Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Access more than 700 courses by purchasing a plan',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
           ),
         ],
       ),
